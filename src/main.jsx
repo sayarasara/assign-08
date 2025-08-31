56d1d72560e2b10bb50829f3dc10df8260d9f95c
sayarasara <sarasayara87@gmail.com>
2025-09-01 01:05:37 +0600

    add Root.jsx file

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+   
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />,
+  </StrictMode>,
+)
